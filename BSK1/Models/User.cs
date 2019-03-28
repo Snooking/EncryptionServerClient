c79c61080e2c39528cef7cbddd80a6a6e0e53668
Snooking <adriankon123@wp.pl>
2019-03-28 16:16:41 +0100

    Finished Sending View

---
+++ BSK1/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace BSK1.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
--- BSK1/ViewModels/SendViewModel.cs
+++ BSK1/ViewModels/SendViewModel.cs
@@ -1,9 +1,26 @@
-﻿using System.Collections.ObjectModel;
+﻿using BSK1.Models;
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace BSK1.ViewModels
 {
     internal class SendViewModel : BaseViewModel
     {
+        private string _path;
+        public string Path
+        {
+            get => _path;
+            set
+            {
+                _path = value;
+                OnPropertyChanged(nameof(Path));
+            }
+        }
+
         private ObservableCollection<string> _algorithms;
         public ObservableCollection<string> Algorithms
         {
@@ -15,15 +32,95 @@ namespace BSK1.ViewModels
             }
         }
 
+        private string _selectedAlgorithm;
+        public string SelectedAlgorithm
+        {
+            get => _selectedAlgorithm;
+            set
+            {
+                _selectedAlgorithm = value;
+                OnPropertyChanged(nameof(SelectedAlgorithm));
+            }
+        }
+
+        private ObservableCollection<User> _users;
+        public ObservableCollection<User> Users
+        {
+            get => _users;
+            set
+            {
+                _users = value;
+                OnPropertyChanged(nameof(Users));
+            }
+        }
+
+        private User _selectedUser;
+        public User SelectedUser
+        {
+            get => _selectedUser;
+            set
+            {
+                _selectedUser = value;
+                OnPropertyChanged(nameof(SelectedUser));
+            }
+        }
+
+        private int _progress;
+        public int Progress
+        {
+            get => _progress;
+            set
+            {
+                _progress = value;
+                OnPropertyChanged(nameof(Progress));
+            }
+        }
+
+        public RelayCommand ChooseFileCommand { get; set; }
+        public RelayCommand SendFileCommand { get; set; }
+
         public SendViewModel()
         {
+            ChooseFileCommand = new RelayCommand(() =>
+            {
+                var fileDialog = new OpenFileDialog();
+                if ((bool)fileDialog.ShowDialog())
+                {
+                    Path = fileDialog.FileName;
+                }
+            });
+            SendFileCommand = new RelayCommand(SendFile, 
+                (_) => SelectedAlgorithm != null && SelectedUser != null && File.Exists(Path));
             Algorithms = new ObservableCollection<string>
             {
-                "A",
-                "B",
-                "C",
-                "D"
+                "ECB",
+                "CBC",
+                "CFB",
+                "OFB"
             };
+    Users = new ObservableCollection<User>
+            {
+                new User
+                {
+                    Name = "Test1"
+                },
+                new User
+                {
+                    Name = "Test2"
+                }
+            };
+        }
+
+        private void SendFile()
+        {
+            Task.Run(() =>
+            {
+                while (Progress < 100)
+                {
+                    Thread.Sleep(100);
+                    Progress++;
+                }
+            });
         }
     }
 }
